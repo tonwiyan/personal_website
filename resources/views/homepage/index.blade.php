fc8265d9c18bdb26df17536e050e15283709c974
tonwiyan <sn.towni@northsouth.edu>
2020-02-27 02:45:11 +0600

    update html

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -4,13 +4,64 @@
         <meta charset="utf-8">
         <title>Index - Tonni's Website.</title>
         <meta name="description" content="This is an example of a meta description.">
-        <!-- <link rel="stylesheet" type="text/css" href="theme.css"> -->
-
         <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
+        <!-- Bulma -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css" integrity="sha256-D9M5yrVDqFlla7nlELDaYZIpXfFWDytQtiV+TaH6F1I=" crossorigin="anonymous" />
+        <!-- Base CSS -->
+        <link rel="stylesheet" type="text/css" href="css/base.css">
     </head>
     <body>
-        This is the index page of home.
+        <div class="container">
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <b id="site-name">SN Towni</b>
+                    </a>
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        
+                    </div>
+                    <div class="navbar-end">
+                        <a class="navbar-item" href="/">
+                            Home
+                        </a>
+                        <a class="navbar-item" href="gallery">
+                            Gallery
+                        </a>
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                About
+                            </a>
+                            <div class="navbar-dropdown">
+                                <a class="navbar-item">
+                                    Biography
+                                </a>
+                                <a class="navbar-item">
+                                    Contact Me
+                                </a>
+                            </div>
+                        </div>
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <a class="button is-primary">
+                                    <strong>Sign up</strong>
+                                </a>
+                                <a class="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </body>
 </html>
